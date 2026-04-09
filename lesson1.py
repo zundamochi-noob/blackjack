36aa1956209df3b256cd090587f4662fc8e02124
zundamoci <junmurakami0619@gmail.com>
2026-04-09 13:08:14 +0900

    旧バージョン

--- lesson1.py
+++ lesson1.py
@@ -8,13 +8,27 @@ def deal():
         card = 10
     return card
 
+#deal カードを引く時の関数
+
+def is_berst(hand):
+    return sum(hand) > 21
+
+#バーストの判定を追加する関数
+
+def is_blackjack(hand):
+    return sum(hand) == 21
+
+#ブラックジャックの判定を追加する関数
+
+#プレイヤーの処理
+
 hand = []
 hand.append(deal())
 hand.append(deal())
 
-if sum(hand) >21:
+if is_berst(hand):
     print("バースト" + str(sum(hand)))
-elif sum(hand) == 21:
+elif is_blackjack(hand):
     print("ブラックジャック")
 else:
     print("合計:" + str(sum(hand)))
@@ -22,10 +36,26 @@ else:
     while action == "y":
         hand.append(deal())
         print("合計: " + str(sum(hand)))
-        if sum(hand) >= 21:
+        if is_berst(hand):
             print("バースト")
             break
         else:
             action = input("もう一枚引きますか？　(y/n): ")
-#ブラックジャック
-#かぶりあり、1player、1,11選択なし
+
+#ディーラーの処理
+
+dealer_hand = []
+dealer_hand.append(deal())
+dealer_hand.append(deal())
+
+#勝敗判定
+
+while sum(dealer_hand) < 17:
+    dealer_hand.append(deal())
+else:
+    print("----勝敗は----")
+    print("ディーラー：　" + str(sum(dealer_hand)) + "  プレイヤー：　" + str(sum(hand))) 
+if sum(dealer_hand) >= sum(hand):
+    print("ディーラーの勝利")
+else:
+    print("プレイヤーの勝利")
